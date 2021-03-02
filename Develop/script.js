eb97038df7433d5d9839c5b5c566982bf8687829
James Corcoran <jpcsoccer1234@gmail.com>
2021-03-01 23:45:21 -0500

    commented on js file attempting to understad code that was given to me

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//i added lines 5-8 
+generateBtn.addEventListener("click", generatePassword);
+function generatePassword() {
+  window.confirm("this happens when you click that button");
+  return "password";
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var password = generatePassword(); // right now this funtion is undefined
+  var passwordText = document.querySelector("#password"); 
+  
+  
   passwordText.value = password;
+  
+  
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.alert("alert"); //this should be used when a message does not require a response
+// //window.confirm("message"); 
+// window.prompt("input something"); //prompts the user to input text 
